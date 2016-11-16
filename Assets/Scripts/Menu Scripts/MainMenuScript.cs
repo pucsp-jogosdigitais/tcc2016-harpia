ceb41ac6f844c64c983a34676e1f055f36d50b6a
Adriana Oliveira <drika.oly@gmail.com>
2016-11-16 12:43:43 -0200

    Alterações menu

--- Assets/Scripts/Menu Scripts/MainMenuScript.cs
+++ Assets/Scripts/Menu Scripts/MainMenuScript.cs
@@ -5,38 +5,32 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuScript : MonoBehaviour {
 
-    public GameObject PainelConfig, PainelMenu, PainelCustom;
-    public Button BotInicialConfig, BotInicialMenu, BotInicialCustom;
+    public GameObject PainelConfig, PainelMenu, PainelCustom, PainelCreditos, PainelControles;
+    public Button BotInicialConfig, BotInicialMenu, BotInicialCustom, BotInicialCreditos, BotInicialControles;
     public MovieTexture texturaVideo;
 
 	// Use this for initialization
 	void Start () {
         PainelConfig.SetActive(false);
         PainelCustom.SetActive(false);
+        PainelControles.SetActive(false);
+        PainelCreditos.SetActive(false);
         PainelMenu.SetActive(true);
         GameObject myEventSystem = GameObject.Find("EventSystem");
         myEventSystem.GetComponent<UnityEngine.EventSystems.EventSystem>().SetSelectedGameObject(BotInicialMenu.gameObject);
         texturaVideo.loop = true;
         texturaVideo.Play();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 
     public void CustomGame()
     {
         PainelMenu.SetActive(false);
         PainelConfig.SetActive(false);
+        PainelControles.SetActive(false);
+        PainelCreditos.SetActive(false);
         PainelCustom.SetActive(true);
         GameObject myEventSystem = GameObject.Find("EventSystem");
         myEventSystem.GetComponent<UnityEngine.EventSystems.EventSystem>().SetSelectedGameObject(BotInicialCustom.gameObject);
-    }
-
-    public void CarrerGame()
-    {
-
     }
 
     public void LoadCustom(int Players)
@@ -49,15 +43,41 @@ public class MainMenuScript : MonoBehaviour {
     {
         PainelMenu.SetActive(false);
         PainelCustom.SetActive(false);
+        PainelControles.SetActive(false);
+        PainelCreditos.SetActive(false);
         PainelConfig.SetActive(true);
         GameObject myEventSystem = GameObject.Find("EventSystem");
         myEventSystem.GetComponent<UnityEngine.EventSystems.EventSystem>().SetSelectedGameObject(BotInicialConfig.gameObject);
+    }
+
+    public void Controles()
+    {
+        PainelMenu.SetActive(false);
+        PainelCustom.SetActive(false);
+        PainelConfig.SetActive(false);
+        PainelControles.SetActive(true);
+        PainelCreditos.SetActive(false);
+        GameObject myEventSystem = GameObject.Find("EventSystem");
+        myEventSystem.GetComponent<UnityEngine.EventSystems.EventSystem>().SetSelectedGameObject(BotInicialControles.gameObject);
+    }
+
+    public void Creditos()
+    {
+        PainelMenu.SetActive(false);
+        PainelCustom.SetActive(false);
+        PainelConfig.SetActive(false);
+        PainelControles.SetActive(false);
+        PainelCreditos.SetActive(true);
+        GameObject myEventSystem = GameObject.Find("EventSystem");
+        myEventSystem.GetComponent<UnityEngine.EventSystems.EventSystem>().SetSelectedGameObject(BotInicialCreditos.gameObject);
     }
 
     public void Back()
     {
         PainelConfig.SetActive(false);
         PainelCustom.SetActive(false);
+        PainelControles.SetActive(false);
+        PainelCreditos.SetActive(false);
         PainelMenu.SetActive(true);
         GameObject myEventSystem = GameObject.Find("EventSystem");
         myEventSystem.GetComponent<UnityEngine.EventSystems.EventSystem>().SetSelectedGameObject(BotInicialMenu.gameObject);
