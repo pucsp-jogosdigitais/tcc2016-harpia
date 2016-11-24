0b3413bcce5be8bbfaef600093eeae9ed052e012
Adriana Oliveira <drika.oly@gmail.com>
2016-11-24 16:33:35 -0200

    Video na tela inicial

--- Assets/Scripts/Menu Scripts/TelaInicialScript.cs
+++ Assets/Scripts/Menu Scripts/TelaInicialScript.cs
@@ -2,12 +2,15 @@
 using System.Collections;
 using UnityEngine.SceneManagement;
 
-public class TelaInicialScript : MonoBehaviour {
+public class TelaInicialScript : MonoBehaviour
+{
+    public MovieTexture texturaVideo;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    // Use this for initialization
+    void Start () {
+        texturaVideo.loop = true;
+        texturaVideo.Play();
+    }
 	
 	// Update is called once per frame
 	void Update () {
