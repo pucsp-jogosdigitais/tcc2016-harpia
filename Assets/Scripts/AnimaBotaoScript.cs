86fd7fa17319f033132ae274804581e37da3b3cf
Adriana Oliveira <drika.oly@gmail.com>
2016-11-24 14:51:32 -0200

    Animacao botoes

--- Assets/Scripts/AnimaBotaoScript.cs
+++ Assets/Scripts/AnimaBotaoScript.cs
@@ -10,15 +10,19 @@ public class AnimaBotaoScript : MonoBehaviour, ISelectHandler, IDeselectHandler/
 	public void OnSelect (BaseEventData eventData) 
 	{
 		Debug.Log (this.gameObject.name + " was selected");
-		ImagemBtnRect.offsetMin.x = ImagemBtnRect.offsetMin.x + 65f; //Left
-		ImagemBtnRect.offsetMax.x = ImagemBtnRect.offsetMax.x + 65f; //Right
-	}
+        ImagemBtnRect.offsetMin = new Vector2(ImagemBtnRect.offsetMin.x + 65f, ImagemBtnRect.offsetMin.y);
+        ImagemBtnRect.offsetMax = new Vector2(ImagemBtnRect.offsetMax.x + 65f, ImagemBtnRect.offsetMax.y);
+        //ImagemBtnRect.offsetMin.x = ImagemBtnRect.offsetMin.x + 65f; //Left
+        //ImagemBtnRect.offsetMax.x = ImagemBtnRect.offsetMax.x + 65f; //Right
+    }
 
 	public void OnDeselect (BaseEventData eventData) 
 	{
 		Debug.Log (this.gameObject.name + " was Deselected");
-		ImagemBtnRect.offsetMin.x = ImagemBtnRect.offsetMin.x - 65f; //Left
-		ImagemBtnRect.offsetMax.x = ImagemBtnRect.offsetMax.x - 65f; //Right
+        ImagemBtnRect.offsetMin = new Vector2(ImagemBtnRect.offsetMin.x - 65f, ImagemBtnRect.offsetMin.y);
+        ImagemBtnRect.offsetMax = new Vector2(ImagemBtnRect.offsetMax.x - 65f, ImagemBtnRect.offsetMax.y);
+        //ImagemBtnRect.offsetMin.x = ImagemBtnRect.offsetMin.x - 65f; //Left
+		//ImagemBtnRect.offsetMax.x = ImagemBtnRect.offsetMax.x - 65f; //Right
 	}
 
 	// Use this for initialization
