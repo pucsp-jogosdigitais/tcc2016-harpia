e2a56e9503306dd2a7bfd7984c4d51ef16c51fc5
Adriana Oliveira <drika.oly@gmail.com>
2016-11-24 17:46:55 -0200

    Animação dos botoes ainda tá cagada

--- Assets/Scripts/AnimaBotaoScript.cs
+++ Assets/Scripts/AnimaBotaoScript.cs
@@ -4,30 +4,45 @@ using UnityEngine.EventSystems;
 
 public class AnimaBotaoScript : MonoBehaviour, ISelectHandler, IDeselectHandler// required interface when using the OnSelect method.
 {
-	private RectTransform ImagemBtnRect;
+    private RectTransform ImagemBtnRect;
+    private float RightSelectedValor, LeftSelectedValor, RightDeselectedValor, LeftDeselectedValor;
+    public bool Selecionado = false;
 
-	//Do this when the selectable UI object is selected.
-	public void OnSelect (BaseEventData eventData) 
-	{
-		Debug.Log (this.gameObject.name + " was selected");
-        ImagemBtnRect.offsetMin = new Vector2(ImagemBtnRect.offsetMin.x + 65f, ImagemBtnRect.offsetMin.y);
-        ImagemBtnRect.offsetMax = new Vector2(ImagemBtnRect.offsetMax.x + 65f, ImagemBtnRect.offsetMax.y);
-        //ImagemBtnRect.offsetMin.x = ImagemBtnRect.offsetMin.x + 65f; //Left
-        //ImagemBtnRect.offsetMax.x = ImagemBtnRect.offsetMax.x + 65f; //Right
+    //Do this when the selectable UI object is selected.
+    public void OnSelect(BaseEventData eventData)
+    {
+        ImagemBtnRect.offsetMin = new Vector2(LeftSelectedValor, ImagemBtnRect.offsetMin.y); //Left
+        ImagemBtnRect.offsetMax = new Vector2(RightSelectedValor, ImagemBtnRect.offsetMax.y); //Right
+        Selecionado = true;
     }
 
-	public void OnDeselect (BaseEventData eventData) 
-	{
-		Debug.Log (this.gameObject.name + " was Deselected");
-        ImagemBtnRect.offsetMin = new Vector2(ImagemBtnRect.offsetMin.x - 65f, ImagemBtnRect.offsetMin.y);
-        ImagemBtnRect.offsetMax = new Vector2(ImagemBtnRect.offsetMax.x - 65f, ImagemBtnRect.offsetMax.y);
-        //ImagemBtnRect.offsetMin.x = ImagemBtnRect.offsetMin.x - 65f; //Left
-		//ImagemBtnRect.offsetMax.x = ImagemBtnRect.offsetMax.x - 65f; //Right
-	}
+    public void OnDeselect(BaseEventData eventData)
+    {
+        ImagemBtnRect.offsetMin = new Vector2(LeftDeselectedValor, ImagemBtnRect.offsetMin.y); //Left
+        ImagemBtnRect.offsetMax = new Vector2(RightDeselectedValor, ImagemBtnRect.offsetMax.y); //Right
+        Selecionado = false;
+    }
 
-	// Use this for initialization
-	void Start () {
-		ImagemBtnRect = GetComponentInChildren<RectTransform> ();
-	
-	}
+    // Use this for initialization
+    void Start()
+    {
+        ImagemBtnRect = GetComponentInChildren<RectTransform>();
+        RightSelectedValor = ImagemBtnRect.offsetMax.x + 65f;
+        LeftSelectedValor = ImagemBtnRect.offsetMin.x + 65f;
+        RightDeselectedValor = ImagemBtnRect.offsetMax.x - 65f;
+        LeftDeselectedValor = ImagemBtnRect.offsetMin.x - 65f;
+    }
+
+    void Update()
+    {
+        if (Selecionado)
+        {
+            if (EventSystem.current.currentSelectedGameObject.gameObject != gameObject)
+            {
+                ImagemBtnRect.offsetMin = new Vector2(LeftDeselectedValor, ImagemBtnRect.offsetMin.y); //Left
+                ImagemBtnRect.offsetMax = new Vector2(RightDeselectedValor, ImagemBtnRect.offsetMax.y); //Right
+                Selecionado = false;
+            }
+        }
+    }
 }
